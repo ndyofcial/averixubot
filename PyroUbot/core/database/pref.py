a153597322a25947e071cbd74b649734185f30db
ndyofcial <ndytelegram@gmail.com>
2025-08-22 17:07:26 +0700

    pref.py

--- PyroUbot/core/database/pref.py
+++ PyroUbot/core/database/pref.py
@@ -2,18 +2,23 @@ from PyroUbot.core.database import mongodb
 
 prefixes = mongodb["PyroUbot"]["prefix"]
 
-async def get_pref(user_id: int):
+# ambil prefix user
+async def get_pref(user_id: int) -> str:
     sh = await prefixes.find_one({"_id": user_id})
     if sh:
-        return sh.get("prefixesi")
-    else:
-        return "."
+        prefix = sh.get("prefixesi")
+        if prefix is None or prefix.lower() == "none":
+            return ""  # kosong = tanpa prefix
+        return prefix
+    return "."  # fallback default
 
+# set prefix
 async def set_pref(user_id: int, prefix: str):
     await prefixes.update_one(
         {"_id": user_id}, {"$set": {"prefixesi": prefix}}, upsert=True
     )
 
+# hapus prefix
 async def rem_pref(user_id: int):
     await prefixes.update_one(
         {"_id": user_id}, {"$unset": {"prefixesi": ""}}, upsert=True
